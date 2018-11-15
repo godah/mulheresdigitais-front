9431e34a177ef70776c16bef9f37d3ad746a104c
Luciano Lima <luciano.fernando@gmail.com>
2018-11-15 09:38:26 -0200

    Sidebar para vermelho

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -32,7 +32,7 @@ class Sidebar extends React.Component {
   }
   render() {
     return (
-      <div className="sidebar" data-color="blue">
+      <div className="sidebar" data-color="red">
         <div className="logo">
           <a
             href="/"
